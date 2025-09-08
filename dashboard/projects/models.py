4e42f4122c5452863b9101e31e200343361b8abc
Daniele Procida <daniele@vurt.org>
2025-09-08 19:21:27 +0100

    Added "candidate" and "not applicable" statuses of conditions.

--- dashboard/projects/models.py
+++ dashboard/projects/models.py
@@ -129,7 +129,7 @@ class ProjectObjective(models.Model):
                 objective=self.objective,
                 condition__level=level,
             )
-            if results.exists() and not results.filter(done=False).exists():
+            if results.exists() and not results.filter(done=False, not_applicable=False).exists():
                 return level
 
     def status(self):
