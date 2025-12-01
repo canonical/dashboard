84a8015f47a08fee5c798fdc2fe51d23e02037f5
David Wilding <david.wilding@canonical.com>
2025-12-01 19:50:17 +0800

    require "change cycle" permission to apply QI values

--- dashboard/framework/views.py
+++ dashboard/framework/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 
 from django.urls import reverse
 from django.contrib import messages
+from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponse, HttpResponseRedirect
 
 from projects.models import Project, QI
 
 
+@permission_required("framework.change_workcycle")
 def admin_apply_qis(request, workcycle_id):
     qis = QI.objects.filter(workcycle_id=workcycle_id).select_related("project")
     for qi in qis:
