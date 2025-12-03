df41df76b22ba463aa1d58c8c196d3fb4e4f3e7c
David Wilding <david.wilding@canonical.com>
2025-12-03 15:41:45 +0800

    order projects by name in filter

--- dashboard/projects/admin.py
+++ dashboard/projects/admin.py
@@ -115,8 +115,16 @@ class CommitmentAdmin(admin.ModelAdmin):
 class ProjectObjectiveConditionAdmin(admin.ModelAdmin):
     list_filter = ["project", "objective", "condition", "status"]
 
+
+class FilterFieldOrderByName(admin.filters.RelatedFieldListFilter):
+    def field_choices(self, field, request, model_admin):
+        objects_by_name = field.remote_field.model._default_manager.order_by("name")
+        return [(obj.pk, str(obj)) for obj in objects_by_name]
+
+
 @admin.register(QI)
 class QIAdmin(admin.ModelAdmin):
-    list_filter = ["project"]
+    list_filter = [("project", FilterFieldOrderByName)]
+
 
 admin.site.register(ProjectGroup)
