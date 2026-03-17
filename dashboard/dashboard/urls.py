e6beb952074027eda9e16f61e236f906609150ba
nbellol <nbellolugo97@gmail.com>
2026-03-17 10:38:55 -0500

    solve error with current paths for local OIDC

--- dashboard/dashboard/urls.py
+++ dashboard/dashboard/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     
-    # OIDC URLs without trailing slashes
+    # OIDC URLs
     path("oidc/authenticate", oidc_views.OIDCAuthenticationRequestView.as_view(), name="oidc_authentication_init"),
     path("oidc/callback", oidc_views.OIDCAuthenticationCallbackView.as_view(), name="oidc_authentication_callback"),
-    path("oidc/logout", oidc_views.OIDCLogoutView.as_view(), name="oidc_logout"),
+    path("oidc/logout/", oidc_views.OIDCLogoutView.as_view(), name="oidc_logout"),
 
     # Dashboard
     path("", include("projects.urls")),
