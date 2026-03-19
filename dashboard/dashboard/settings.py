828a9abb0c454c42be2336541d7cdf957d2a1e7c
nbellol <nbellolugo97@gmail.com>
2026-03-19 14:37:11 -0500

    solve pr comments

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -87,7 +87,7 @@ OIDC_AUTHENTICATION_CALLBACK_URL = os.environ.get("DJANGO_OIDC_AUTHENTICATION_CA
 OIDC_RP_SIGN_ALGO = "RS256"
 OIDC_RP_SCOPES = "openid email profile"
 OIDC_TOKEN_USE_BASIC_AUTH = True  # Use client_secret_basic instead of client_secret_post
-OIDC_LOGIN_BUTTON_TEXT = "Canonical Login"  # Customize this text as needed
+OIDC_LOGIN_BUTTON_TEXT = "Your Company Login"  # Customize this text as needed
 
 # Session settings for OIDC
 OIDC_RENEW_ID_TOKEN_EXPIRY_SECONDS = 15 * 60  # 15 minutes
