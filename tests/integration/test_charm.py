50e41eec3362c6f7000159fc37d054bdd72fcd20
Ali Ugur <aliugur997@gmail.com>
2026-04-09 22:31:17 +0300

    chore: delete unrelated tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -28,30 +28,4 @@ def test_dashboard_health(dashboard_app: App, juju: jubilant.Juju) -> None:
             timeout=20,
         )
         assert res.status_code == 200
-        assert b"<title>Home | Dashboard</title>" in res.content
-
-        # Also some random thing from the static dir.
-        url = f"http://{unit.address}:8000/static/dashboard.ico"
-        res = requests.get(
-            url,
-            timeout=20,
-        )
-        assert res.status_code == 200
-
-
-@pytest.mark.usefixtures("dashboard_app")
-def test_dashboard_rq_worker_running(juju: jubilant.Juju, dashboard_app: App) -> None:
-    """
-    arrange: Build and deploy the Dashboard charm.
-    act: Do a get request to the status api.
-    assert: Check that there is one rq worker running.
-    """
-    status = juju.status()
-    for unit in status.apps[dashboard_app.name].units.values():
-        url = f"http://{unit.address}:8000/api/status/"
-        res = requests.get(
-            url,
-            timeout=20,
-        )
-        assert res.status_code == 200
-        assert res.json()["rq-workers-running"] == 1
+        assert b"<title>Dashboard</title>" in res.content
