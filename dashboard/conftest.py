35f08a2a725b3fab2c696d5a2ec543ba9b510bfc
David Wilding <david.wilding@canonical.com>
2026-04-26 08:16:21 +0800

    remove OIDC testing shim

--- dashboard/conftest.py
+++ dashboard/conftest.py
@@ -1,6 +1,3 @@
-import sys
-import types
-
 import pytest
 
 from django.contrib.auth.models import Permission, User
@@ -85,27 +82,3 @@ def user_is_staff(client):
     )
     client.login(username="staffmember", password="password")
     return user
-
-
-# Creat a "fake" mozilla_django_oidc.views so that tests will run,
-# even if mozilla_django_oidc is not available at import time for
-# tests.
-
-if "mozilla_django_oidc" not in sys.modules:
-    oidc_module = types.ModuleType("mozilla_django_oidc")
-    oidc_views_module = types.ModuleType("mozilla_django_oidc.views")
-
-    class _DummyOIDCView:
-        @classmethod
-        def as_view(cls):
-            def _view(request, *args, **kwargs):
-                return HttpResponse("")
-
-            return _view
-
-    oidc_views_module.OIDCAuthenticationRequestView = _DummyOIDCView
-    oidc_views_module.OIDCAuthenticationCallbackView = _DummyOIDCView
-    oidc_views_module.OIDCLogoutView = _DummyOIDCView
-    oidc_module.views = oidc_views_module
-    sys.modules["mozilla_django_oidc"] = oidc_module
-    sys.modules["mozilla_django_oidc.views"] = oidc_views_module
